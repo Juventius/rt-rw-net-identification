4d7e03cc512100443153b706b0dd8b72d99598eb
Juventius <85542712+Juventius@users.noreply.github.com>
2024-08-05 10:53:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def haversine(lat1, lon1, lat2, lon2):
 st.title("RT/RW Net Identification")
 st.markdown("made by Juventius Kriswijanarko \([LinkedIn](https://www.linkedin.com/in/juventius-kriswijanarko/)\)")
 st.markdown("*Please contact the above person for feedbacks and suggestions.")
-st.markdown("*[Sample Datasets](https://drive.google.com/drive/folders/1loDyXbKmJ31iNdCnSLI1wIkhD_8nstZS?usp=sharing) (current access for erickhimura@gmail.com)")
+st.markdown("*[Sample Datasets](https://drive.google.com/drive/folders/1loDyXbKmJ31iNdCnSLI1wIkhD_8nstZS?usp=sharing) (access granted for erickhimura@gmail.com)")
 
 ookla_file = st.file_uploader("Upload Ookla Excel Dataset:", type=["xlsx"])
 odp_file = st.file_uploader("Upload ODP Excel Dataset:", type=["xlsx"])
